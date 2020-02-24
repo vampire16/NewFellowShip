d4b77d83488995204cf2fd47d343ccb98ed90b8d
vampire16 <siddheshsalvi525@gmai.com>
2020-02-24 12:18:24 +0530

    ADDED : Number to day

---
+++ caseselection/NumToDay.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+#USER INPUT
+read -p "enter number between 0-6 : " day
+
+case $day in
+0) echo "Sunday" ;;
+1) echo "Monday" ;;
+2) echo "Tuesday" ;;
+3) echo "Wednesday" ;;
+4) echo "Thursday" ;;
+5) echo "Friday" ;;
+6) echo "Saturday" ;;
+*) echo "Invalid"
+esac
