c0bcf75ba70fd54f49e1f534e5fe514f3a116c80
vampire16 <siddheshsalvi525@gmai.com>
2020-02-24 12:19:14 +0530

    ADDED : Enter number it will print in words

---
+++ caseselection/NumToWords.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+#USER INPUT
+read -p "enter single digit number : " digit
+
+case $digit in
+0) echo "Zero" ;;
+1) echo "One" ;;
+2) echo "Two" ;;
+3) echo "Three" ;;
+4) echo "Four" ;;
+5) echo "Five" ;;
+6) echo "Six" ;;
+7) echo "Seven" ;;
+8) echo "Eight" ;;
+9) echo "Nine" ;;
+*) echo "Invalid"
+esac
