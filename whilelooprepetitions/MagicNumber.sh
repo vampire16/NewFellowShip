1fe5768add5252fb3b343faa1e1befb0fc7b6ad3
vampire16 <siddheshsalvi525@gmai.com>
2020-02-24 14:22:06 +0530

    ADDED : Magic number

---
+++ whilelooprepetitions/MagicNumber.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+#RANDOM NUMBER
+number=$(( (RANDOM%100)+1 ))
+
+#CONSTANT VARIABLES
+FIRST=0
+LAST=100
+MID=$(( $(($FIRST+$LAST))/2 ))
+
+#CHECKING RANDOM NUMBER IS PRESENT OR NOT
+while [ $FIRST -le $LAST ]
+do
+	if [ $MID -lt $number ]
+	then
+		FIRST=$(($MID+1))
+	elif [ $MID -eq $number ]
+	then
+		echo "got the number $number"
+ 		break
+	else
+		LAST=$(($MID-1))
+	fi
+MID=$(( $(($FIRST+$LAST))/2 ))
+done
