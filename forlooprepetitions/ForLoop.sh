2c3d7f56c279ffd5ea4b8d82cf4ed4a1fe5274ae
vampire16 <siddheshsalvi525@gmai.com>
2020-02-24 13:00:51 +0530

    ADDED : For Loop Test

---
+++ forlooprepetitions/ForLoop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x 
+
+for file in `ls *.txt`;
+do
+	foldername=`echo $file | awk -F. '{print $1}'`;
+	if [ -d $foldername ];
+	then
+	  rm -R $foldername;
+	fi
+	mkdir $foldername;
+	cp $file $foldername;
+	rm $file;
+done
